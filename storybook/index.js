b7646f11f0d32813350ebdb12cf0e35615db9c34
robophil <oghenerobo@gmail.com>
2018-08-05 15:57:12 +0100

    test: fix tests

--- storybook/index.js
+++ storybook/index.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react'
-import { AppRegistry, NativeModules } from 'react-native'
+import { AppRegistry } from 'react-native'
 import { getStorybookUI, configure } from '@storybook/react-native'
-import url from 'url'
 
 // import stories
 configure(() => {
   require('./stories')
 }, module)
 
-const { hostname } = url.parse(NativeModules.SourceCode.scriptURL)
-
 // This assumes that storybook is running on the same host as your RN packager,
 // to set manually use, e.g. host: 'localhost' option
-const StorybookUIRoot = getStorybookUI({ host: hostname, port: 7007, onDeviceUI: true })
+const StorybookUIRoot = getStorybookUI({ port: 7007, onDeviceUI: true })
 
 // react-native hot module loader must take in a Class - https://github.com/facebook/react-native/issues/10991
 // https://github.com/storybooks/storybook/issues/2081
